6880756f7cb232a2ff20e16eeb5b281820dbf686
John Enrick Plenos <johnenrick@localhost.localdomain>
2020-01-29 21:47:17 +0800

    Dashboard Daily Reports (current sales, total transactions, and total items sold)

--- src/views/dashboard/QuickReportCards.vue
+++ src/views/dashboard/QuickReportCards.vue
@@ -6,7 +6,7 @@
         <div class="card-body text-center; p-3">
           <div style="width:50px';float:left"><h1><fa icon="money-bill"/></h1></div>
           <div class="pl-2 text-center" style="width: calc(100% - 50px); float:left">
-            <h5 class="card-title  font-weight-bold mb-0">47,357.00</h5>
+            <h5 class="card-title  font-weight-bold mb-0">{{currentSales | numberToMoney}}</h5>
             <p class="card-text p-0"><small>Current Sales</small></p>
           </div>
         </div>
@@ -17,7 +17,7 @@
         <div class="card-body text-center; p-3">
           <div style="width:50px';float:left"><h1><fa icon="receipt"/></h1></div>
           <div class="pl-2 text-center" style="width: calc(100% - 50px); float:left">
-            <h5 class="card-title  font-weight-bold mb-0">100</h5>
+            <h5 class="card-title  font-weight-bold mb-0">{{totalTransactions}}</h5>
             <p class="card-text p-0"><small>Total Transactions</small></p>
           </div>
         </div>
@@ -28,7 +28,7 @@
         <div class="card-body text-center; p-3">
           <div style="width:50px';float:left"><h1><fa icon="box"/></h1></div>
           <div class="pl-2 text-center" style="width: calc(100% - 50px); float:left">
-            <h5 class="card-title  font-weight-bold mb-0">232</h5>
+            <h5 class="card-title  font-weight-bold mb-0">{{totalSold}}</h5>
             <p class="card-text p-0"><small>Items Sold</small></p>
           </div>
         </div>
@@ -37,8 +37,56 @@
   </div>
 </template>
 <script>
-
+import Transaction from '@/database/controller/transaction'
 export default {
+  data(){
+    return{
+      totalSold: 0,
+      totalTransactions: 0,
+      currentSales: 0
+    }
+  },
+  mounted(){
+    this._initialize()
+  },
+  methods: {
+    _initialize(){
+      this.generateReport()
+    },
+    generateReport(){
+      let transaction = {}
+      let startDateFilter = new Date()
+      startDateFilter.setHours(0)
+      startDateFilter.setMinutes(0)
+      startDateFilter.setSeconds(1)
+      let endDateFilter = new Date()
+      endDateFilter.setHours(23)
+      endDateFilter.setMinutes(59)
+      endDateFilter.setSeconds(59)
+      let query = {
+        where: {
+          created_at: {
+            '>': startDateFilter.getTime(),
+            '<': endDateFilter.getTime(),
+          },
+          status: 1
+        },
+        with: {
+          transaction_products: {}
+        }
+      };
+      (new Transaction()).get(query).then((response) => {
+        transaction = response || []
+        for(let x in transaction){
+          this.totalTransactions++
+          this.currentSales = this.currentSales + (transaction[x]['total_amount'] * 1)
+          for(let y in transaction[x]['transaction_products']){
+            this.totalSold = this.totalSold + (transaction[x]['transaction_products'][y]['quantity'] * 1)
+          }
+        }
+      })
+    }
+  },
 
 }
 </script>
