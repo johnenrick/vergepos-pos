a687ccd9f28f4ed4a24a4524b2972ac8bac2f066
John Enrick Plenos <johnenrick@localhost.localdomain>
2020-02-11 20:53:23 +0800

    dasdas

--- src/helpers/toggle-fullscreen.js
+++ src/helpers/toggle-fullscreen.js
@@ -1,13 +1,21 @@
+let isFullscreen = false
 let elem = document.documentElement
-let toggle = () => {
+let toggle = (isFull = null) => {
+  if(isFull === null){
+    isFullscreen = !isFullscreen
+  }else if(isFull === true){
+    isFullscreen = true
+  }else if(isFull === 'default'){
+    isFullscreen = false
+  }
   if (elem.webkitRequestFullscreen) {
     elem.webkitRequestFullscreen()
   }
   if (!window.screenTop && !window.screenY) {
     document.webkitExitFullscreen()
   }
+  return isFullscreen
 }
-
 export default {
   toggle: toggle
 }
