a67a44276ccc4ff35ba43dc23fd3f6c235a40d33
Kenji Barral <kenjibarral2@gmail.com>
2020-04-02 13:35:24 +0800

    not found = changed h5 to p with 18px font size

--- src/views/error/NotFound.vue
+++ src/views/error/NotFound.vue
@@ -5,13 +5,13 @@
     </div>
     <h2 class="mb-4 text-center">Page not Found</h2>
     <div class="row">
-      <div class="col-1"></div>
-      <div class="col-10 bg-light pl-3 pr-3 pt-3">
+      <div class="col-3"></div>
+      <div class="col bg-light pt-3 text-center" style="font-size: 18px">
         <span>
-          <h5><fa class="text-info" icon="info-circle" /> If you think this is an error, send us a message at <a class="text-info" href="https://web.facebook.com/pg/vergepos/">https://web.facebook.com/vergepos/</a></h5>
+          <p><fa class="text-info" icon="info-circle" /> If you think this is an error, send us a message at <a class="text-info" href="https://web.facebook.com/pg/vergepos/">https://web.facebook.com/vergepos/</a></p>
         </span>
       </div>
-      <div class="col-1"></div>
+      <div class="col-3"></div>
     </div>
   </div>
 </template>
