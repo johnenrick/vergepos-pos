44a9c9c89954a71b97e2f2645362ff8cd7af5bca
Kenji Barral <kenjibarral2@gmail.com>
2020-04-16 02:50:23 +0800

    Product performance graphs - added title and graph brief decscription

--- src/views/terminal_reports/product-performance-components/YearlyLineGraph.vue
+++ src/views/terminal_reports/product-performance-components/YearlyLineGraph.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="w-100">
     <div class="row">
-        <div class="col">
-        <button @click="switchDisplay(1)" class="btn mt-4 ml-3" :class="view === 1 ? ' btn-primary' : ' btn-outline-primary'">View Quantity</button>
-        </div>
-        <div class="col">
-        <button @click="switchDisplay(2)" class="btn mt-4 ml-3" :class="view === 2 ? ' btn-primary' : ' btn-outline-primary'">View Amount</button>
-        </div>
-        <div class="col">
-        <button @click="switchDisplay(3)" class="btn mt-4 ml-3" :class="view === 3 ? ' btn-primary' : ' btn-outline-primary'">View Discount Amount</button>
-        </div>
-        <div class="col">
-        <button @click="switchDisplay(4)" class="btn mt-4 ml-3" :class="view === 4 ? ' btn-primary' : ' btn-outline-primary'">View Profit</button>
+      <div class="col ml-4 mt-4">
+        <h5>Yearly</h5>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col mt-2 ml-4">
+        <p>This graph shows all the transactions made for every hour within the selected timeframe.
+          It identifies which products are sold the most in every year.
+          </p>
+      </div>
+    </div>
+    <div class="text-center">
+      <div class="row mt-2">
+        <div class="col btn-group-sm">
+          <button @click="switchDisplay(1)" class="btn" :class="view === 1 ? ' btn-primary' : ' btn-outline-primary'">Quantity</button>
+          <button @click="switchDisplay(2)" class="btn" :class="view === 2 ? ' btn-primary' : ' btn-outline-primary'">Amount</button>
+          <button @click="switchDisplay(3)" class="btn" :class="view === 3 ? ' btn-primary' : ' btn-outline-primary'">Discount Amount</button>
+          <button @click="switchDisplay(4)" class="btn" :class="view === 4 ? ' btn-primary' : ' btn-outline-primary'">Profit</button>
         </div>
       </div>
+    </div>
     <line-chart v-if="datacollection" :chart-data="datacollection" :options="chartConfig" :styles="{responsive: true, position: 'relative'}"></line-chart>
   </div>
 </template>
